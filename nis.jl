663ca1f7cf591b60391c29e290220ad0620b77ec
Matheus Kautzmann <mkvchain@gmail.com>
2020-06-23 14:50:28 -0300

    Adiciona opcoes

--- nis.jl
+++ nis.jl
@@ -5,6 +5,7 @@ include("portScanner.jl")
 include("smtpScan.jl")
 include("httpScan.jl")
 include("scanDirs.jl")
+include("scanLinks.jl")
 
 using ArgParse
 
@@ -32,6 +33,16 @@ function parse_arguments()
         "--smtp"
         help = "Run SMTP server scan for users"
         action = :store_true
+        "--files"
+        help = "Run HTTP scan for common directories and files"
+        action = :store_true
+        "--links"
+        help = "Run link scan on HTTP server"
+        action = :store_true
+        "--depth"
+        help = "Depth for link scanning."
+        arg_type = Int
+        default = 2
     end
     return parse_args(s)
 end
@@ -41,13 +52,16 @@ function scan()
     target = get(parsed_args, "target", "")
     if target == ""
         print("\nNo target was informed!")
-        exit(1)
+    exit(1)
     end
     ip = get_ip_from_host(target)
     start = coalesce(get(parsed_args, "start", missing), START_PORT)
     finish = coalesce(get(parsed_args, "end", missing), END_PORT)
+    depth = coalesce(get(parsed_args, "depth", missing), 2)
     only_open = coalesce(get(parsed_args, "open", missing), false)
     run_smtp = coalesce(get(parsed_args, "smtp", missing), false)
+    run_dirs = coalesce(get(parsed_args, "files", missing), false)
+    run_links = coalesce(get(parsed_args, "links", missing), false)
     print_banner()
     print("\n")
     print("Running port scan on target $ip\n")
@@ -60,10 +74,13 @@ function scan()
     http_conn, url = http_scan(ip, openPorts)
     server = get_server(http_conn)
     print("\nHTTP Server: $server\n")
-    if url !== nothing
+    if url !== nothing && run_dirs
         scan_dirs(url)
     end
-    # TODO: Adicionar scan de links na página
+    if url !== nothing && run_links
+        scan_links(url, depth)
+    end
+    print("\n\nDone! See you next time!")
 end
 
 # Start scanning
